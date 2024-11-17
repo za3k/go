b496b83abb014c47347babd68e6141c850ea820a
Zachary Vance <za3k@za3k.com>
2024-11-16 20:51:59 -0500

    Add capture rules

--- go-engine.js
+++ go-engine.js
@@ -1,3 +1,27 @@
+class IllegalMove extends Error {
+    constructor(message, options) { super(message, options) }
+}
+
+class PosSet {
+    constructor() {
+        this.s = {}
+    }
+    add(...poses) {
+        for (var pos of poses)
+            (this.s[pos.y] ||= {})[pos.x] = true
+    }
+    has(pos) {
+        return (this.s[pos.y] || {})[pos.x]
+    }
+    poses() {
+        const poses = []
+        for (var y in this.s)
+            for (var x in this.s[y])
+                poses.push({x: Number(x), y: Number(y)})
+        return poses
+    }
+}
+
 class Engine {
     constructor(size) {
         this.player = 0 // whose turn is it
@@ -11,27 +35,148 @@ class Engine {
         }
         this.lastMove = {x:-1, y:-1}
         this.done = false
+        this.passes = 0
+        this.komi = 7.5
     }
 
-    resign() {}
-    pass() {}
+    otherPlayer(p) {
+        return (p+1)%2
+    }
+
+    resign() {
+        this.done = true
+        this.victor = this.otherPlayer(this.player)
+        this.resigned = true
+    }
+
+    pass() {
+        this.passes++
+        this.player = this.otherPlayer(this.player)
+        if (this.passes >= 2) this.done = true
+    }
+
+    clone() {
+        var n = Object.assign(Object.create(Object.getPrototypeOf(this)), this)
+        n.board = structuredClone(this.board)
+        return n
+    }
 
     get(pos) { return this.board[pos.y][pos.x] }
 
-    move(pos) {
-        if (!this.canMove(pos)) return
-        this.board[pos.y][pos.x] = ["black","white"][this.player]
-        this.player = (this.player+1)%2
+    move(pos, hypothetical) {
+        if (!!this.board[pos.y][pos.x]) throw new IllegalMove("That position is not empty")
 
+        const color = ["black","white"][this.player]
+        this.board[pos.y][pos.x] = color
+        
+        var captured = []
+        for (var neighbor of this.neighborsPos(pos)) {
+            if (this.get(neighbor) && this.get(neighbor) != color && this.libertiesGroup(neighbor) == 0) {
+                captured.push(neighbor)
+            }
+        }
+        for (var group of captured) {
+            if (this.get(group)) this.captureGroup(group)
+            this.captureGroup(group)
+        }
 
+        if (this.libertiesGroup(pos) == 0) throw new IllegalMove("Suicide is not allowed")
+
+        // TODO: Disallow ko
+
+        this.player = this.otherPlayer(this.player)
+        this.passes = 0
+        this.lastMove = pos
     }
+
+    neighborsPos(pos) { // Neighbors of the position
+        var neighbors = []
+        const {x, y} = pos
+        if (x > 0)           neighbors.push({x: x-1, y})
+        if (x < this.size-1) neighbors.push({x: x+1, y})
+        if (y > 0)           neighbors.push({x, y: y-1})
+        if (y < this.size-1) neighbors.push({x, y: y+1})
+        return neighbors
+    }
+
+    neighborsGroup(group) { // Neighbors of the group
+        var neighbors = new PosSet()
+        for (var pos of this.positionsGroup(group)) {
+            neighbors.add(...this.neighborsPos(pos))
+        }
+        return neighbors.poses()
+    }
+
+    captureGroup(pos) { // Capture the group
+        if (!this.get(pos)) return
+        for (var pos of this.positionsGroup(pos)) this.board[pos.y][pos.x] = null
+    }
+
+    positionsGroup(group) {
+        const color = this.get(group)
+
+        var visited = new PosSet()
+        var q = []
+        function queue(pos) {
+            if (visited.has(pos)) return
+            visited.add(pos)
+            q.push(pos)
+        }
+
+        queue(group)
+        while (q.length > 0) {
+            const n = q.shift()
+            for (var neighbor of this.neighborsPos(n)) {
+                if (this.get(neighbor) == color) queue(neighbor)
+            }
+        }
+
+        return visited.poses()
+    }
+
+    libertiesGroup(group) { // Liberties of the group (as a number)
+        var liberties = 0
+        for (var n of this.neighborsGroup(group)) {
+            if (!this.get(n)) liberties++
+        }
+        return liberties
+    }
+
+    uniqueGroups(groups) { // Return unique groups out of the list
+        return groups // TODO, not needed yet
+    }
+
+    uniquePos(poses) {
+        // TODO: Not used
+        return new PosSet().add(...poses).poses()
+    }
+
     canMove(pos) {
-        if (!!this.get(pos)) return false
-        return true // TODO: disallow suicides, ko
+        if (!!this.get(pos)) return false // Slight speedup
+        const v2 = this.clone()
+        try {
+            v2.move(pos, true)
+            return true
+        } catch(err) {
+            if (err instanceof IllegalMove) return false
+            else {
+                console.error(err.stack)
+                throw err
+            }
+        }
     }
 
     // Scoring
     toggleDead(pos) {}
-    get score() {}
+    finishScoring(player) {
+        // TODO: require both players to click it
+        this.resigned = false
+        if (this.score > 0) this.victor = 0
+        else                this.victor = 1 // White wins ties, arbitrarily
+    }
+    get score() { // Positive is "for black"
+        var score = -this.komi
+        return score // TODO
+    } 
 
 }
--- go-ui.js
+++ go-ui.js
@@ -52,23 +52,40 @@ const boards = {
 var spritesPromise
 const spriteNames = ["┌","┬","┐","├","┼","┤","└","┴","┘",".","white","whiteRecent","whiteDead","black","blackRecent","blackDead"]
 const maxSize = Math.min(window.innerWidth, window.innerHeight) / 19
-console.log(maxSize)
 if (maxSize > 60) {
     spritesPromise = Sprites.loadAll("sprites60.png", 60, spriteNames)
+} else if (maxSize > 45) {
+    spritesPromise = Sprites.loadAll("sprites45.png", 45, spriteNames)
 } else if (maxSize > 30) {
     spritesPromise = Sprites.loadAll("sprites30.png", 30, spriteNames)
 } else {
     spritesPromise = Sprites.loadAll("sprites15.png", 15, spriteNames)
 }
     
+function randInt(min, max) { return Math.floor(Math.random()*(max-min)) + min }
 
 class Game {
     constructor(size, online) {
         this.online = online
         this.engine = new Engine(size)
 
-        $(".action.resign").on("click", () => { this.resign() })
-        $(".action.pass").on("click", () => { this.pass() })
+        $(".action.resign").on("click", () => { 
+            this.engine.resign()
+            this.updateUI() 
+        })
+        $(".action.pass").on("click", () => {
+            this.engine.pass()
+            this.updateUI()
+        })
+        $(".action.finish-scoring").on("click", () => {
+            if (this.online)
+                this.engine.finishScoring(this.me)
+            else {
+                this.engine.finishScoring(0)
+                this.engine.finishScoring(1)
+            }
+            this.updateUI()
+        })
 
         spritesPromise.then((sprites) => {
             this.sprites = sprites
@@ -105,15 +122,13 @@ class Game {
         $(sprite).addClass("possible-move").appendTo(tile).on("click", callback)
     }
 
+    playClack() {
+        const i = randInt(1, 5) // 1-4
+        this.playSound(`clack${i}`)
+    }
     playSound(name) {
         const audio = new Audio(`audio/${name}.mp3`)
         audio.play()
-    }
-
-    win(winner) {
-        this.playSound("win")
-        const sprite = this.sprites[["black", "white"][winner]]
-        $("#win").show().text(`${["Black", "White"][winner]} wins`).prepend(sprite.make()).append(sprite.make())
     }
 
     eachPos(f) {
@@ -123,32 +138,61 @@ class Game {
             }
         }
     }
+    
+    score() {
+        if (this.engine.resigned) {
+            return `${"BW"[this.engine.victor]}+R`
+        }
+
+        const score = this.engine.score
+        return `${"BW"[0+(score <= 0)]}+${Math.abs(score)}`
+    }
 
     updateUI() {
-        $(".sprite .sprite").remove()
+        const playerSprites = [this.sprites["black"], this.sprites["white"]]
+        if (this.engine.done && typeof(this.engine.victor) == 'undefined') {
+            // Scoring phase
+            $("#turn-info").text(`Scoring`).prepend(playerSprites[0].make()).append(playerSprites[1].make())
+            $(".my-turn").hide()
+            $(".finish-scoring").show()
+            $("#score").show().text(this.score())
+            $(".possible-move").remove()
+        } else if (this.engine.done) {
+            // Game is over
+            const sprite = this.sprites[["black", "white"][this.engine.victor]]
+            $("#win").show().text(`${["Black", "White"][this.engine.victor]} wins`).prepend(sprite.make()).append(sprite.make())
+            $("#win, #turn-info").show().text(`${["Black", "White"][this.engine.victor]} wins`).prepend(sprite.make()).append(sprite.make())
+            $(".my-turn").css("visibility", "hidden")
+            $(".finish-scoring").hide()
+            $("#score").show().text(`Final score: ${this.score()}`)
+            $(".possible-move").remove()
+        } else {
+            // Regular play
+            const myTurn = this.online && this.me == this.player || !this.online
+            const color = ["black", "white"][this.player]
 
-        this.eachPos((pos) => {
-            const sprite = this.engine.get(pos)
-            this.setSprite(pos, sprite)
-        })
+            $("#turn-info").text(`${["Black", "White"][this.player]}'s turn`)
+            const sprite = playerSprites[this.player]
+            $("#turn-info").prepend(playerSprites[this.player].make()).append(playerSprites[this.player].make())
 
-        $("#turn-info").text(`${["Black", "White"][this.player]}'s turn`)
-        const color = ["black", "white"][this.player]
-        const sprite = this.sprites[color]
-        $("#turn-info").prepend(sprite.make()).append(sprite.make())
+            $(".my-turn").css("visibility", myTurn ? "" : "hidden")
 
-        if (this.online && this.me == this.player || !this.online) {  // My turn
+            $(".sprite .sprite").remove()
             this.eachPos((pos) => {
-                if (this.engine.canMove(pos)) { 
+                const sprite = this.engine.get(pos)
+                this.setSprite(pos, sprite)
+
+                if (myTurn && this.engine.canMove(pos)) {
                     this.addPreview(pos, color, () => {
                         this.engine.move(pos)
+                        this.playClack()
                         this.updateUI()
                     })
                 }
             })
-        } else { // Not my turn
-
         }
+
+
     }
 }
 
